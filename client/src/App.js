7e9ba3fb52fd18c97e79f4ea1e2e5225492f4c77
cdnphantom <cduynguyen95@gmail.com>
2019-10-27 19:53:41 -0400

    auth0 implementation in react

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,8 @@
 // src/App.js
 
 import React from "react";
-import NavBar from "./components/NavBar";
 import { useAuth0 } from "./react-auth0-spa";
+import SignInSide from "./pages/SignInSide";
 
 function App() {
   const { loading } = useAuth0();
@@ -16,7 +16,7 @@ function App() {
   return (
     <div className="App">
       <header>
-        <NavBar />
+        <SignInSide />
       </header>
     </div>
   );
