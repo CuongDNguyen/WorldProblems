cbc0ab03218c28bcf1f72b07efebc27a120571d5
cdnphantom <cduynguyen95@gmail.com>
2019-10-27 18:43:28 -0400

    heroku deploy

--- server.js
+++ server.js
@@ -1,10 +1,23 @@
+require('dotenv').config();
 const express = require('express');
 const app = express();
 
+const db = require('./models');
+
 const PORT = process.env.PORT || 8080;
 
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
 app.use(express.static('client/build'));
 
-app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`)
-})
+require("./routes/apiRoutes")(app);
+
+const syncOptions = { force: true }
+
+db.sequelize.sync(syncOptions).then(function() {
+    app.listen(PORT, () => {
+        console.log(`Server is running on port ${PORT}.`)
+    });
+  });
+  
+  module.exports = app;
